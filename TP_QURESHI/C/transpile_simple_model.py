fe576e2fd530c4092e0665119900622d75229505
Hammad Qureshi <38525501+HMQ1107@users.noreply.github.com>
2023-04-07 18:54:20 +0200

    details

---
+++ TP_QURESHI/C/transpile_simple_model.py
@@ -0,0 +1,4 @@
+import joblib as jb
+
+
+model = jb.load("model.joblib")
---
+++ TP_QURESHI/Verilog/Linear_Reg/linear_reg.v
@@ -0,0 +1,8 @@
+module linear_reg (size,prize,cout);
+    input [15:0] size;
+    input [15:0] price;
+
+    output [31:0] cout;
+
+
+endmodule : linear_reg
